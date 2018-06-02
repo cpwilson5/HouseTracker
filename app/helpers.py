b57ec30a0f6b266773f7d6fd15568ef180708e33
Chris <cpwilson5@gmail.com>
2018-06-02 11:01:02 -0400

    test

--- app/helpers.py
+++ app/helpers.py
@@ -1,7 +1,7 @@
 from flask import flash, url_for
 from flask import current_app as app
 from itsdangerous import URLSafeSerializer
-from .utils import send_email
+from .utils import send_email, test
 
 def flash_errors(form):
     """Flashes form errors"""
