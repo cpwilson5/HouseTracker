829dd261ac79bb56368202dd2b86dbab2f40589d
Chris <cpwilson5@gmail.com>
2018-06-23 11:29:14 -0400

    Ability to upload listing photo, restyle listing cards, add days left

--- app/utils.py
+++ app/utils.py
@@ -10,10 +10,6 @@ from flask_mail import Message
 from app import mail
 
 def s3_upload(source_file, upload_dir=None, acl='public-read'):
-    #uncomment if we start wanting to use folders
-    #if upload_dir is None:
-    #    upload_dir = app.config["S3_UPLOAD_DIRECTORY"]
-
     source_filename = secure_filename(source_file.data.filename)
     source_extension = os.path.splitext(source_filename)[1]
 
@@ -24,8 +20,7 @@ def s3_upload(source_file, upload_dir=None, acl='public-read'):
     b = conn.get_bucket(app.config["S3_BUCKET"])
 
     #uncomment if we want to start using folders
-    #sml = b.new_key("/".join([upload_dir, destination_filename]))
-    sml = b.new_key("/".join([destination_filename]))
+    sml = b.new_key("/".join([upload_dir, destination_filename]))
     sml.set_contents_from_string(source_file.data.read())
     sml.set_acl(acl)
 
@@ -36,7 +31,7 @@ def s3_retrieve(key, upload_dir=None, acl='public-read'):
     conn = boto.connect_s3(app.config["S3_KEY"], app.config["S3_SECRET"])
     b = conn.get_bucket(app.config["S3_BUCKET"])
 
-    sml = b.get_key(key)
+    sml = b.get_key("/".join([upload_dir, key]))
     url = sml.generate_url(3600, query_auth=True, force_http=True)
     return url
 
