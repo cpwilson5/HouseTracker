b09e4be6ee720b021c1b8245944a1562b44a888a
Chris <cpwilson5@gmail.com>
2018-07-13 08:13:53 -0400

    add configs to production

--- config.py
+++ config.py
@@ -44,6 +44,33 @@ class ProductionConfig(Config):
     Production configurations
     """
 
+    # mongo
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
+    MONGO_URI = os.environ.get('MONGO_URI')
+
+    # s3
+    S3_LOCATION = os.environ.get('S3_LOCATION')
+    S3_KEY = os.environ.get('S3_KEY')
+    S3_SECRET = os.environ.get('S3_SECRET')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
+
+    # email server
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    # email confirmation key
+    MAIL_SECURITY_PASSWORD_SALT = os.environ.get('MAIL_SECURITY_PASSWORD_SALT')
+
+    # sms
+    TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
+
     DEBUG = False
 
 app_config = {
