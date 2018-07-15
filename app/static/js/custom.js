a9a728655d9d5589196c7e30020cab10bbf75feb
Chris <cpwilson5@gmail.com>
2018-07-15 07:46:39 -0400

    fix csrf on sort for ajax call (meta header) fix status dropdown positioning on mobile hide sort on completed listings

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -13,6 +13,16 @@ $(document).ready(function(){
       }
   });
 
+  // sets the csrf token using a meta tag
+  var csrf_token = $('meta[name=csrf-token]').attr('content');
+  $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+          if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+              xhr.setRequestHeader("X-CSRFToken", csrf_token);
+          }
+      }
+  });
+
   $("#sortable").sortable('disable');
 
   $('#sortorder').on('click',function(){
